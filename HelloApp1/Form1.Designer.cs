33d65e4fb3c49edffdb70fb387f4e8eaf27a94c7
Aleksandrfreem <aleksandrfreem@gmail.com>
2022-06-11 16:53:02 +0300

    Добавьте файлы проекта.

---
+++ HelloApp1/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace HelloApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(78, 101);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Who is it?";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.BackgroundImage = global::HelloApp1.Properties.Resources.wpapers_ru_Johnny_Depp;
+            this.ClientSize = new System.Drawing.Size(1113, 669);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Metanit";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button button1;
+    }
+}
---
+++ HelloApp1/Form1.cs
@@ -0,0 +1,16 @@
+namespace HelloApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Click += button1_Click;
+        }
+
+        private void button1_Click(object? sender, EventArgs e)
+        {
+            MessageBox.Show("Johnny Depp");
+        }
+            }
+}
